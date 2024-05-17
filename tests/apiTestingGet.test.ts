c4885b88679402c4c5823f0a62c5fa31adf0abd0
ManishKumar-18 <manish.kumar@vshsolutions.com>
2024-05-17 11:31:08 +0530

    added some test cases and also modified github action file

---
+++ tests/apiTestingGet.test.ts
@@ -0,0 +1,54 @@
+import { test, expect } from "@playwright/test";
+import { request } from "http";
+
+test("Testing Rest api with get method", async ({ request }) => {
+  const url = `https://jsonplaceholder.typicode.com/posts/1`;
+  const res = await request.get(url);
+  expect(res.status()).toBe(200);
+
+  const responseBody = await res.json();
+  console.log(responseBody);
+  expect(responseBody.userId).toBe(1);
+});
+
+test.only("Testing Rest api with post method", async ({ request }) => {
+  const url = `https://jsonplaceholder.typicode.com/posts`;
+  const res = await request.post(url, {
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    data: {
+      title: "New Post",
+      body: "post4567",
+      userId: 5,
+    },
+  });
+  expect(res.status()).toBe(201);
+
+  const responseBody = await res.json();
+  console.log(responseBody);
+  expect(responseBody.title).toBe("New Post");
+});
+
+test('Api Chaining', async({request}) => {
+
+    //get all posts
+    const postResponse = await request.get('https://jsonplaceholder.typicode.com/posts');
+    expect(postResponse.status()).toBe(200);
+
+    //parse response body to json
+
+    const posts = await postResponse.json();
+
+    const firstPost = posts[0];
+
+    //get comments from the select post
+
+    const commentsResponse = await request.get(`https://jsonplaceholder.typicode.com/posts/${firstPost.id}/comments`);
+    expect(commentsResponse.status()).toBe(200);
+
+
+    const comments = await commentsResponse.json();
+    expect(comments.length).toBeGreaterThan(0);
+    expect(comments[0].postid).toBe(firstPost.id);
+});
---
+++ tests/basicInteractive.test.ts
@@ -0,0 +1,39 @@
+import test, { expect } from "@playwright/test";
+
+// test("Interaction with inputs",async({page}) => {
+//     await page.goto("https://www.lambdatest.com/selenium-playground/simple-form-demo");
+//     // await page.getByPlaceholder('Please enter your Message').hover;
+//     const messageInput = page.locator("input#user-message");
+//     console.log(await messageInput.getAttribute("placeholder"));
+//     expect(messageInput).toHaveAttribute("placeholder","Please enter your Message");
+//     console.log(await messageInput.inputValue());
+//     await messageInput.type("Hello Manish")
+//     await page.waitForTimeout(5000);
+// })
+
+// test("Checkbox",async({page}) => {
+//     await page.goto("https://www.lambdatest.com/selenium-playground/checkbox-demo");
+//     const singleCheckbox = page.locator("id=isAgeSelected");
+//     expect(singleCheckbox).not.toBeChecked();
+//     await singleCheckbox.check();
+//     expect(singleCheckbox).toBeChecked();
+// })
+
+test("handling alerts",async({page}) => {
+    await page.goto("https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo");
+
+    page.on('dialog',async (alert) => {
+        const text = alert.message();
+        console.log(text);
+        await alert.accept()
+    })
+    await page.locator("button:has-text('Click Me')").nth(0).click();
+})
+
+
+test("handling dropdown",async({page}) => {
+    await page.goto("https://www.lambdatest.com/selenium-playground/javascript-select-dropdown-demo");
+    await page.selectOption("#select-demo",{
+        label:'Tuesday'
+    })
+})
